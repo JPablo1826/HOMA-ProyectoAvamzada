c2194a16d6be454079fb2580980f9e53718104f6
JPablo1826 <juanp.castrov@uqvirtual.edu.co>
2025-09-10 21:02:44 -0500

    Proyecto HOMA - backend Spring Boot + frontend Angular inicial

---
+++ Homa/src/main/java/poo/uniquindio/edu/co/Homa/HomaApplication.java
@@ -0,0 +1,13 @@
+package poo.uniquindio.edu.co.Homa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomaApplication.class, args);
+	}
+
+}
---
+++ Homa/src/test/java/poo/uniquindio/edu/co/Homa/HomaApplicationTests.java
@@ -0,0 +1,13 @@
+package poo.uniquindio.edu.co.Homa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
