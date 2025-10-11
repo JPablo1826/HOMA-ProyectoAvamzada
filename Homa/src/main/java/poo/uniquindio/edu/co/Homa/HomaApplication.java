09adfba29cf61ee50b8f72390e4ea686b3939406
Emili666 <emili.garciab@uqvirtual.edu.co>
2025-10-11 17:27:06 -0500

    cambio de nombre

--- Homa/src/main/java/poo/uniquindio/edu/co/Homa/HomaApplication.java
+++ Homa/src/main/java/poo/uniquindio/edu/co/Homa/HomaApplication.java
@@ -1,8 +1,5 @@
-
 package poo.uniquindio.edu.co.homa;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,9 +7,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "poo.uniquindio.edu.co.Homa.repository")
-@EntityScan(basePackages = "poo.uniquindio.edu.co.Homa.model")
-@ComponentScan(basePackages = "poo.uniquindio.edu.co.Homa")
+@EnableJpaRepositories(basePackages = "poo.uniquindio.edu.co.homa.repository")
+@EntityScan(basePackages = "poo.uniquindio.edu.co.homa.model")
+@ComponentScan(basePackages = "poo.uniquindio.edu.co.homa")
 public class HomaApplication {
 
     public static void main(String[] args) {
